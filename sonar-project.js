ce23f4c1282454f9f35317d340711f0152a0bcd8
chuks0684 <122063699+chuks0684@users.noreply.github.com>
2023-04-11 19:18:18 -0700

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -7,7 +7,7 @@ sonarqubeScanner({
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
 	    'sonar.login': 'love',
-	    //'sonar.password': 'class31',
+	    'sonar.password': 'class31',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
